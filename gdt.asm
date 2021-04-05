cbe65f991a37b9a9ed67791925a83d563b73c46a
marchersimon <marchersimon@zohomail.eu>
2021-04-05 21:22:07 +0200

    enter 32-bit protected mode

---
+++ gdt.asm
@@ -0,0 +1,32 @@
+gdt_start:
+
+gdt_null:   ; mandatory null descriptor
+    dd 0    ; dd = double word (4 Bytes)
+    dd 0
+    
+gdt_code:
+    dw 0xffff   ; limit (bits 0 - 15)
+    dw 0x0      ; base (bits 16 - 31)
+    db 0x0      ; base (bits 0 - 7)
+    db 10011010b    ; segment type: readable code, descriptor type: code or data, privilege = 0, present = 1
+    db 11001111b    ; segment limit: 0xffff, avl: not used, 64-bit: 0, 32-bit segment, granularity: 1
+    db 0x0  ; another base
+    
+gdt_data:
+    dw 0xffff   ; limit (bits 0 - 15)
+    dw 0x0      ; base (bits 16 - 31)
+    db 0x0      ; base (bits 0 - 7)
+    db 10010010b    ; segment type: readable code, descriptor type: system, privilege = 0, present = 1
+    db 11001111b    ; segment limit: 0xffff, avl: not used, 64-bit: 0, 32-bit segment, granularity: 1
+    db 0x0  ; another base
+    
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+    
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+    
