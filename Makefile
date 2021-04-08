3664aadf4f7cb0e6f0787b9fc2161f0e39ed75ec
marchersimon <marchersimon@zohomail.eu>
2021-04-08 19:00:21 +0200

    Fix compilation and linking

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 main:
 	nasm boot.asm -o build/boot.bin
-	gcc -ffreestanding -c kernel.c -o build/kernel.o
-	ld -o build/kernel.bin -Ttext 0x1000 build/kernel.o --oformat binary
+	nasm kernel_entry.asm -f elf -o build/kernel_entry.o
+	gcc -fno-pie -m32 -ffreestanding -c kernel.c -o build/kernel.o
+	ld -m elf_i386 -o build/kernel.bin -Ttext 0x1000 build/kernel_entry.o build/kernel.o --oformat binary
 	cat build/boot.bin build/kernel.bin > os.iso
 	truncate -s 1M os.iso # os.iso must not be bigger than 1 MiB
 	qemu-system-x86_64 os.iso
--- kernel.c
+++ kernel.c
@@ -3,37 +3,39 @@ void main () {
     
     video_memory += 480;  
     
+	
+	
     for (char i = 0; i < 26; i++) {
-        video_memory[2*i+4] = 0x50;
+        video_memory[2*i+1] = 0x50;
     }
     
-
-    video_memory[1] = 'S';
-    video_memory[3] = 'u';
-    video_memory[5] = 'c';
-    video_memory[7] = 'c';
-    video_memory[9] = 'e';
-    video_memory[11] = 's';
-    video_memory[13] = 's';
-    video_memory[15] = 'f';
-    video_memory[17] = 'u';
-    video_memory[19] = 'l';
-    video_memory[21] = 'l';
-    video_memory[23] = 'y';
-    video_memory[25] = ' ';
-    video_memory[27] = 'l';
-    video_memory[29] = 'o';
-    video_memory[31] = 'a';
-    video_memory[33] = 'd';
-    video_memory[35] = 'e';
-    video_memory[37] = 'd';
-    video_memory[39] = ' ';
-    video_memory[41] = 'k';
-    video_memory[43] = 'e';
-    video_memory[45] = 'r';
-    video_memory[47] = 'n';
-    video_memory[49] = 'e';
-    video_memory[51] = 'l';
+	
+    video_memory[0] = 'S';
+    video_memory[2] = 'u';
+    video_memory[4] = 'c';
+    video_memory[6] = 'c';
+    video_memory[8] = 'e';
+    video_memory[10] = 's';
+    video_memory[12] = 's';
+    video_memory[14] = 'f';
+    video_memory[16] = 'u';
+    video_memory[18] = 'l';
+    video_memory[20] = 'l';
+    video_memory[22] = 'y';
+    video_memory[24] = ' ';
+    video_memory[26] = 'l';
+    video_memory[28] = 'o';
+    video_memory[30] = 'a';
+    video_memory[32] = 'd';
+    video_memory[34] = 'e';
+    video_memory[36] = 'd';
+    video_memory[38] = ' ';
+    video_memory[40] = 'k';
+    video_memory[42] = 'e';
+    video_memory[44] = 'r';
+    video_memory[46] = 'n';
+    video_memory[48] = 'e';
+    video_memory[50] = 'l';
     
     
     
