cf716c98c90a8eea7bde8cf474155cb029258c7c
marchersimon <marchersimon@zohomail.eu>
2021-04-10 23:48:54 +0200

    enhance printing functions

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,5 @@
-char* cursorPosition = (char*) 0xb8000;
-int x = 4;
+char* videoMemory = 0xb8000;
+int cursorPosition = 0;
 
 void skipLines(int lines) {
 	for(int i = 0; i < lines; i++) {
@@ -9,21 +9,41 @@ void skipLines(int lines) {
 
 int printf(char* string) {
 	for(int i = 0; string[i]; i++) {
-		*cursorPosition = string[i];
+		
+		if(string[i] == '\n') {
+			cursorPosition = (((int) (cursorPosition) + 160 - 1) / 160) * 160;
+			continue;
+		}
+		videoMemory[cursorPosition] = string[i];
+		videoMemory[cursorPosition+1] = 0x07;
 		cursorPosition += 2;
 	}
 	return 0;
 }
 
+void clearScreen() {
+	for(int i = 0; i < 3000 /*4000*/; i++) {
+		videoMemory[i] = 0;
+	}
+	cursorPosition = 0;
+}
+
 void main () {
 	
 	skipLines(3);
-	
-	char b[] = "Successfully loadad";
-	char c[] = " kernel";
-	
-	printf(b);
-	
-	printf(" kernel");
+	printf("Successfully loaded kernel\n");
+	printf("Welcome to tldrOS\n");
+	printf("      ___           ___       ___           ___           ___           ___     \n");
+	printf("     /\\  \\         /\\__\\     /\\  \\         /\\  \\         /\\  \\         /\\  \\    \n");
+	printf("     \\:\\  \\       /:/  /    /::\\  \\       /::\\  \\       /::\\  \\       /::\\  \\   \n");
+	printf("      \\:\\  \\     /:/  /    /:/\\:\\  \\     /:/\\:\\  \\     /:/\\:\\  \\     /:/\\ \\  \\  \n");
+	printf("      /::\\  \\   /:/  /    /:/  \\:\\__\\   /::\\~\\:\\  \\   /:/  \\:\\  \\   _\\:\\~\\ \\  \\ \n");
+	printf("     /:/\\:\\__\\ /:/__/    /:/__/ \\:|__| /:/\\:\\ \\:\\__\\ /:/__/ \\:\\__\\ /\\ \\:\\ \\ \\__\\\n");
+	printf("    /:/  \\/__/ \\:\\  \\    \\:\\  \\ /:/  / \\/_|::\\/:/  / \\:\\  \\ /:/  / \\:\\ \\:\\ \\/__/\n");
+	printf("   /:/  /       \\:\\  \\    \\:\\  /:/  /     |:|::/  /   \\:\\  /:/  /   \\:\\ \\:\\__\\  \n");
+	printf("   \\/__/         \\:\\  \\    \\:\\/:/  /      |:|\\/__/     \\:\\/:/  /     \\:\\/:/  /  \n");
+	printf("                  \\:\\__\\    \\::/__/       |:|  |        \\::/  /       \\::/  /   \n");
+	printf("                   \\/__/     ~~            \\|__|         \\/__/         \\/__/    \n");
 
+	//clearScreen();
 }
